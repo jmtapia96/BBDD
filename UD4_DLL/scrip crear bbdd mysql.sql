69657b0999dad17c2dc44895354ce93d3b0bfc1f
jmtapia96 <jmtapia96@gmail.com>
2025-12-15 14:10:23 +0100

    Se termina el 2 del bol 1 de DML

--- UD4_DLL/scrip crear bbdd mysql.sql
+++ UD4_DLL/scrip crear bbdd mysql.sql
@@ -1,3 +1,1 @@
 create database b3ej2;
-create user 'dummy'@'%' identified by 'dummy';
-grant all on b3ej2.* to 'dummy'@'%';
--- UD5_DML/Boletin1_DML/DmlBol1Ej2.sql
+++ UD5_DML/Boletin1_DML/DmlBol1Ej2.sql
@@ -29,7 +29,7 @@ CREATE TABLE PERTENECE
 CREATE TABLE PRUEBA
 (
 	nombreprueba VARCHAR2(20),
-	anio NUMBER(5),
+	anio DATE,
 	kmtotales NUMBER(5),
 	ciclistaganador VARCHAR2(40),
 
@@ -37,14 +37,81 @@ CREATE TABLE PRUEBA
 	CONSTRAINT FK1_PRUEBA FOREIGN KEY  (ciclistaganador) REFERENCES CICLISTA (nombreciclista)
 );
 
+--INSERTS
+
+
 --Tabla EQUIPO
+INSERT INTO EQUIPO (nombreequipo) VALUES('Movistar');
+INSERT INTO EQUIPO (nombreequipo) VALUES('Jumbo');
+INSERT INTO EQUIPO (nombreequipo) VALUES('Bora');
+INSERT INTO EQUIPO (nombreequipo) VALUES('UAE Team');
+
 
 --Tabla CICLISTA
-INSERT INTO CICLISTA VALUES ('');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('Alejandro Valverde',TO_DATE ('1980/04/25','YYYY/MM/DD'),'Español');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('Alejandra Valverde',TO_DATE ('1980/04/25','YYYY/MM/DD'),'Español');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('Mathias Norsgaard',TO_DATE ('1997/05/01','YYYY/MM/DD'),'Danés');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('Primox Roglic',TO_DATE ('1997/01/01','YYYY/MM/DD'),'Esloveno');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('R. Gesink',TO_DATE ('2003/05/01','YYYY/MM/DD'),'Holandés');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('S. Bennett',TO_DATE ('2000/08/03','YYYY/MM/DD'),'Irlandés');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('M. Soler',TO_DATE ('2003/11/09','YYYY/MM/DD'),'Español');
+INSERT INTO CICLISTA (nombreciclista,fnacimiento,nacionalidad) VALUES ('JM. Muñoz',TO_DATE ('1996/05/12','YYYY/MM/DD'),'San Jose de la Rda');
 
 
 --Tabla PERTENECE
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('Alejandro Valverde','Movistar');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('Alejandra Valverde','Movistar');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('Mathias Norsgaard','Movistar');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('Primox Roglic','Jumbo');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('R. Gesink','Jumbo');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('S. Bennett','Bora');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('M. Soler','UAE Team');
+INSERT INTO PERTENECE (nombreciclista,nombreequipo) VALUES ('JM. Muñoz','UAE Team');
+
 
 --Tabla PRUEBA
 
-INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES();
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E1_2022',TO_DATE('2022','YYYY'),23,'R. Gesink');
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E2_2022',TO_DATE('2022','YYYY'),175,'Primox Roglic');
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E3_2022',TO_DATE('2022','YYYY'),193,'S. Bennett');
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E1_2021',TO_DATE('2021','YYYY'),154,'S. Bennett');
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E2_2021_2',TO_DATE('2021','YYYY'),189,'M. Soler');
+--El nombre de prueba de este ultimo insert es igual que el anterior, por lo que fallaria y se modifica levemente para que no de error
+INSERT INTO PRUEBA (nombreprueba,anio,kmtotales,ciclistaganador) VALUES('E1_2020',TO_DATE('2020','YYYY'),40,'Alejandro Valverde');
+
+
+--UPDATES
+
+--Tabla PRUEBA
+
+UPDATE PRUEBA SET nombreprueba='Contra_'||TO_CHAR(anio,'YYYY') WHERE kmtotales<100;
+
+
+--3.
+
+CREATE TABLE ciclistasEspanoles
+(
+	nombreciclista VARCHAR2(40),
+	nombreequipo VARCHAR2(20),
+
+	CONSTRAINT PK_CICLISTAS_ESP PRIMARY KEY (nombreciclista),
+	CONSTRAINT FK1_CICLISTAS_ESP FOREIGN KEY (nombreciclista) REFERENCES CICLISTA(nombreciclista),
+	CONSTRAINT FK2_CICLISTAS_ESP FOREIGN KEY (nombreequipo) REFERENCES EQUIPO(nombreequipo)
+);
+
+--INSERTS
+
+--Tabla ciclistasEspanoles
+
+INSERT INTO ciclistasEspanoles (nombreciclista,nombreequipo)
+SELECT P.nombreciclista, P.nombreequipo FROM PERTENECE P
+JOIN CICLISTA C ON P.NOMBRECICLISTA = C.NOMBRECICLISTA
+WHERE C.nacionalidad='Español';
+
+
+
+SELECT * FROM PERTENECE;
+SELECT * FROM CICLISTA;
+SELECT * FROM EQUIPO;
+SELECT * FROM PRUEBA;
+SELECT * FROM ciclistasEspanoles;
